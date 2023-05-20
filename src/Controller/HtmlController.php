614e5ea59b7ff6c97d5a5167f5a31d63c96fcc90
= <bendev4977@gmail.com>
2023-05-21 01:42:40 +0200

    update

--- src/Controller/HtmlController.php
+++ src/Controller/HtmlController.php
@@ -2,15 +2,40 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Html;
+use App\Form\HtmlFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HtmlController extends AbstractController
 {
     #[Route('/html', name: 'app_html')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $manager): Response
     {
-        return $this->render('pages/html/html.html.twig');
+        $html = new Html();
+
+        $form = $this->createForm(HtmlFormType::class, $html);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $html = $form->getData();
+
+            $manager->persist($html);
+            $manager->flush();
+
+            $this->addFlash('success', 'Your HTML message have been successfully sent !');
+            return $this->redirectToRoute('app_html');
+        }
+
+        else if ($form->isSubmitted() && !$form->isValid())
+        {
+            $this->addFlash('warning', 'Complete the following step and try again.');
+        }
+
+        return $this->render('pages/html/html.html.twig', compact('form'));
     }
 }
---
+++ src/Form/HtmlFormType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Html;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class HtmlFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, options:[
+                'attr' => [
+                    'minlength' => '3',
+                    'maxlength' => '30'
+                ],
+                'label' => 'Nom'
+            ])
+            ->add('submit', SubmitType::class, options:[
+                'attr' => [
+                    'class' => 'button-send'
+                ],
+                'label' => 'Envoyer',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Html::class,
+        ]);
+    }
+}
